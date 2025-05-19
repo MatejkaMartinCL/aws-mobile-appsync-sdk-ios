22e48fe16fccf830a0ee8477f19c300ae71b849b
MatejkaMartinCL <marmat@ciklum.com>
2025-05-19 10:11:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(
             name: "AppSyncRealTimeClient",
             url: "https://github.com/aws-amplify/aws-appsync-realtime-client-ios.git",
-            .upToNextMinor(from: "3.2.0")
+            .upToNextMinor(from: "3.1.2")
         ),
         .package(
             url: "https://github.com/stephencelis/SQLite.swift.git",
